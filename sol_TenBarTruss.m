a2525e6783aa717279b6da8ac039d4a920466104
Kao1121 <willy2512689@gmail.com>
2024-09-01 16:04:13 +0900

    Update sol_TenBarTruss.m

--- sol_TenBarTruss.m
+++ sol_TenBarTruss.m
@@ -6,17 +6,10 @@ function [stress,Q_reduced] = sol_TenBarTruss(x1, x2)
     ele_con=[3 5;1 3;4 6;2 4;3 4;1 2;4 5;3 6;2 3;1 4];    %elements' connection
     num_ele=size(ele_con,1);    %number of elements
     ele_dof=[5 6 9 10;1 2 5 6;7 8 11 12;3 4 7 8;5 6 7 8;1 2 3 4;7 8 9 10;5 6 11 12;3 4 5 6;1 2 7 8];    %elements' dof
+
     %A, E
-    A(1)=x1^(2)*pi;
-    A(2)=x1^(2)*pi;
-    A(3)=x1^(2)*pi;
-    A(4)=x1^(2)*pi;
-    A(5)=x1^(2)*pi;
-    A(6)=x1^(2)*pi;
-    A(7)=x2^(2)*pi;
-    A(8)=x2^(2)*pi;
-    A(9)=x2^(2)*pi;
-    A(10)=x2^(2)*pi;
+    A(1:6)=x1^(2)*pi;
+    A(7:10)=x2^(2)*pi;
     E=200*10^9;
         
     % 開一個空白的剛性矩陣 (stiffness matrix)
@@ -44,11 +37,11 @@ function [stress,Q_reduced] = sol_TenBarTruss(x1, x2)
     % 建立空白位移矩陣
     Q=zeros(2*num_nod,1);
     % 計算位移量 Q_reduced 
-    F_reduced=F(1:8);
-    K_reduced=K(1:8,1:8);
-    Q_reduced=Q(1:8);
-    Q_reduced=K_reduced^(-1)*F_reduced;
-    Q_reduced(12,1)=0;
+    F_reduced = F(1:8);
+    K_reduced = K(1:8,1:8);
+    Q_reduced = Q(1:8);
+    Q_reduced = K_reduced^(-1)*F_reduced;
+    Q_reduced(12,1) = 0;
 
     % 計算應力 (stress) 
     for e=1:num_ele
@@ -56,9 +49,9 @@ function [stress,Q_reduced] = sol_TenBarTruss(x1, x2)
         C=(nod_coor(ele_con(e,2),1)-nod_coor(ele_con(e,1),1))/L(e);
         S=(nod_coor(ele_con(e,2),2)-nod_coor(ele_con(e,1),2))/L(e);
         stress(e)=(E/L(e))*[-C -S C S]*Q_reduced((ele_dof(e,:))');
-    end
+    %end
 
     % 計算反作用力 R
-    K_reaction=K(9:12,1:12);
-    R=K_reaction*Q_reduced;
+    K_reaction = K(9:12,1:12);
+    R = K_reaction * Q_reduced;
 end
